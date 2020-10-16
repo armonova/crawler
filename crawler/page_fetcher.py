5fb7925a9b4782182ef1ecc906be5140e6c75c6d
Arthur Novaes <arthur.novaes@enacom.com.br>
2020-10-15 22:49:06 -0300

    uptading code and starting the report

--- crawler/page_fetcher.py
+++ crawler/page_fetcher.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 from threading import Thread
 import requests
+import time
 from urllib.parse import urlparse, urljoin
+from .scheduler import Scheduler
 
 
 class PageFetcher(Thread):
@@ -29,9 +31,6 @@ class PageFetcher(Thread):
         Retorna os links do conteúdo bin_str_content da página já requisitada obj_url
         """
         soup = BeautifulSoup(bin_str_content, features="lxml")
-        print('\n')
-        print(soup)
-        print('\n')
         for link in soup.select("a[href]"):
             obj_new_url = urlparse(link.get("href"))
             print(link.get("href"))
@@ -46,14 +45,29 @@ class PageFetcher(Thread):
             Coleta uma nova URL, obtendo-a do escalonador
         """
         next_url = self.obj_scheduler.get_next_url()
-        content = self.request_url(next_url)
-        print(content)
-        depth = 3
-        self.discover_links(next_url, depth, content)
-        pass
+        if next_url is None:
+            time.sleep(Scheduler.TIME_LIMIT_BETWEEN_REQUESTS)
+            return
+        content = self.request_url(next_url[0])
+        depth = next_url[1]
+        urls = self.discover_links(next_url, depth, content)
+
+        aux = []
+        links = []
+        for url in urls:
+            if not url[0].netloc in aux:
+                aux.append(url[0].netloc)
+                links.append(url)
+        for index, (url_link, depth) in enumerate(links):
+            if not url_link is None:
+                self.obj_scheduler.add_new_page(url_link, depth)
+        return True
 
     def run(self):
         """
             Executa coleta enquanto houver páginas a serem coletadas
         """
-        pass
+        [self.obj_scheduler.add_new_page(url) for url in self.obj_scheduler.arr_urls_seeds]
+        while not self.obj_scheduler.has_finished_crawl():
+            self.crawl_new_url()
+            self.obj_scheduler.count_fetched_page()
